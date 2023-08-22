aaffe6aca7cf60ecf99d6b5088980b06c308b087
Joshua Roberts <josh.roberts0527@gmail.com>
2023-08-22 21:10:55 +0000

    08/22/2023

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,25 +12,51 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function search(array, string){
+    let output = null;
+    for(let i = 0; i < array.length; i++){//for loop over array
+        if(array[i].name === string){//if statement comparing name property at each index to string
+             output = array[i];//=>array[i]
+        }
+    }
+        return output;
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function replace(array, name, replacement) { 
+    for(let i =0; i <array.length; i++){// for loop over array
+        if(array[i].name === name){//if statement comparing name to name property at each index of array
+            array[i] = replacement;//replacement becomes the index value in the array
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(array, name){
+    for(let i = 0; i < array.length; i++){//for loop over array
+        if(array[i].name === name){//if statement comparing name to name property ant each index of array
+            array.splice(i, 0);//delete the index of the array
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function add(array, object){
+    for(let i=0; i < array.length; i++){//for loop over array
+        if(array[i].name.length < 0 && array[i].species.length < 0){//if statement comparing if name property at each index and species property at each index has value
+            array.push(object);//adds object to the array
+        }
+    }
+}
 
 
 /**
