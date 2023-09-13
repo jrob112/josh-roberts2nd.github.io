277cd60ba871cae2d7d8d4d7682ce9a0c9ed8074
Joshua Roberts <josh.roberts0527@gmail.com>
2023-09-13 02:50:43 +0000

    LGF complete

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -23,7 +23,6 @@ var _ = require('underbar');
 
 var maleCount = function(array) {
     let count = _.filter(array, (customer) => {return customer.gender === 'male'})
-    console.log(count.length)
     return count.length;
 };
 
@@ -55,21 +54,94 @@ var youngestCustomer = (array) =>{
         return youngest.name;
     };;
 
-var averageBalance = (array) =>{
-    let average = _.reduce(array, (acc, cur) => {
+    var averageBalance = (array) =>{
+   
+        let average = array.reduce((acc, cur) => {
+         acc += parseFloat(cur.balance.replace(/[$,]/g, ''));
+                return acc
+        },0)
+        let x = average / array.length
         
+        return x
+    };
+
+var firstLetterCount = (array, char) => {let count = array.filter((customer) =>
+    customer.name.charAt(0) === char.toUpperCase())
+    return count.length;
+  }
+
+var friendFirstLetterCount = (array, person, char) =>{
+    let count = 0;
+    let user = array.filter((customer) => customer.name === person)  
+      
+      user[0].friends.forEach((friend) => {
+          if(friend.name.charAt(0) === char.toUpperCase()){
+            count += 1;
+      }     
+    })                                             
+      return count;
+    };
+
+    var friendsCount = (array, x) => {
+        let buds = []
+         array.forEach((customer) => {//each customer
+          customer.friends.forEach(friend =>{
+           if(friend.name === x){
+            buds.push(customer.name)}})
+        })
+          return buds;
+      };
+
+      var topThreeTags = (array) => {
+        let output = []
+          array.forEach(customer => {output = output.concat(customer.tags)})
+          let obj = output.reduce((acc, cur)=> {
+            if(acc[cur] === undefined){
+              acc[cur] = 1;
+            }else{
+              acc[cur] += 1;
+            }
+              return acc;
+          }, {})
+        let x = Object.entries(obj)
+        // console.log(x)
+        x.sort((a, b)=>{
+          return b[1] - a[1]
+        })
+        let b = []
+        b.push(x[0][0])
+        b.push(x[1][0])
+        b.push(x[2][0])
+        return b
+      };
+
+var genderCount = (array) =>{
+    let count = {};
+    let females = _.reduce(array, (accumalator, current) => {
+        if(current.gender === 'female'){
+            accumalator += 1;
+        }
+        return accumalator;
     }, 0)
-};
+     count.female = females;
 
-var firstLetterCount;
+    let males = _.reduce(array, (accumalator, current) => {
+        if(current.gender === 'male'){
+            accumalator += 1;
+        }
+        return accumalator;
+    }, 0)
+     count.male = males;
+    let nb = _.reduce(array, (accumalator, current) => {
+        if(current.gender === 'non-binary'){
+            accumalator += 1;
+        }
+        return accumalator;
+    }, 0)
+     count['non-binary'] = nb;
+     return count;
 
-var friendFirstLetterCount;
-
-var friendsCount;
-
-var topThreeTags;
-
-var genderCount;
+}
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
